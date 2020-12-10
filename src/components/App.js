2f129b2e1758626af2af0d1a5bf0d9f87c4cedaf
shariquearshad <shariquearshad@gmail.com>
2020-12-10 23:53:09 +0530

    rendered

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+const description =
+  "an app where you can order from local shop and pickup at set time to avoide queue";
 
-const App = () => {
+export default function App() {
   return (
-    <div id="main"></div>
-  )
+    <div className="App">
+      <h1 data-ns-test="project-name">Local Grocery</h1>
+      <h3 data-ns-test="project-description">{description}</h3>
+    </div>
+  );
 }
-
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+//console.log("rootElement");
